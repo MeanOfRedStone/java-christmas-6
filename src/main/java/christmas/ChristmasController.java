5044baab932f2be7d8d477b0bd7e07919974b680
SeokEui Hong <hongs7701@naver.com>
2023-12-27 00:44:11 +0900

    feat: [#46] "전체 애플리케이션 완성"

--- src/main/java/christmas/ChristmasController.java
+++ src/main/java/christmas/ChristmasController.java
@@ -6,9 +6,11 @@ import christmas.view.InputView;
 
 public class ChristmasController {
     InputView inputView = new InputView();
+    ChristmasService christmasService = new ChristmasService();
 
     public void run(){
         ReservationDate reservationDate = inputView.readDate();
         OrderList orderList = inputView.readOrder();
+        christmasService.showReservation(orderList, reservationDate);
     }
 }
